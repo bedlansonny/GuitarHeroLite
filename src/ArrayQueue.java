ee1bee30e719c28d345c4c64406702c3c7a16f8a
bedlansonny <bedlansonny@gmail.com>
2017-09-05 16:31:33 -0500

    starting on ArrayQueue

--- src/ArrayQueue.java
+++ src/ArrayQueue.java
@@ -1,4 +1,21 @@
+import java.lang.reflect.Array;
+
 public class ArrayQueue
 {
-    
+    double[] arr;
+    int front;
+    int back;
+
+    ArrayQueue(int size)
+    {
+        arr = new double[size];
+        front = 0;
+        back = 0; // will this work???
+    }
+
+    public void enqueue(double value)
+    {
+        arr[back] = value;
+        back++;
+    }
 }
