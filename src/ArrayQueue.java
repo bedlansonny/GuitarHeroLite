91cb1848a0922ebba91c08e796280a1025b050ce
bedlansonny <bedlansonny@gmail.com>
2017-09-07 16:30:30 -0500

    started creating ArrayQueue

--- src/ArrayQueue.java
+++ src/ArrayQueue.java
@@ -10,12 +10,60 @@ public class ArrayQueue
     {
         arr = new double[size];
         front = 0;
-        back = 0; // will this work???
+        back = 0;
     }
 
     public void enqueue(double value)
     {
-        arr[back] = value;
-        back++;
+        if(!isFull())
+        {
+            arr[back] = value;
+            increment(back);
+        }
+        else
+            System.out.println("ERROR: The ArrayQueue is full.");
+    }
+
+    public double dequeue()
+    {
+        increment(front);
+        return arr[front-1];
+    }
+
+    public boolean isEmpty()
+    {
+        return front == back;
+    }
+
+    public boolean isFull()
+    {
+        return (front == 0 && back == arr.length-1) || (back == front - 1);
+    }
+
+    public double peek()
+    {
+        return arr[front];
+    }
+
+    public int size()
+    {
+
+    }
+
+    public String toString()
+    {
+
+    }
+
+    private void increment(int index)
+    {
+        if(index < arr.length-1)
+        {
+            index++;
+        }
+        else
+        {
+            index = 0;
+        }
     }
 }
