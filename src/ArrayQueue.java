092d9b4f87d8f7f4612ab362ef3429ef7cbbdef3
bedlansonny <bedlansonny@gmail.com>
2017-09-07 17:44:30 -0500

    finished ArrayQueue first version

--- src/ArrayQueue.java
+++ src/ArrayQueue.java
@@ -1,69 +1,105 @@
-import java.lang.reflect.Array;
-
 public class ArrayQueue
 {
     double[] arr;
     int front;
     int back;
+    int size;
 
     ArrayQueue(int size)
     {
         arr = new double[size];
-        front = 0;
-        back = 0;
+        front = -1;
+        back = -1;
+        size = 0;
     }
 
     public void enqueue(double value)
     {
         if(!isFull())
         {
-            arr[back] = value;
             increment(back);
+            if(size == 1)
+                increment(front);
+            size++;
+            arr[back] = value;
         }
         else
-            System.out.println("ERROR: The ArrayQueue is full.");
+            System.out.println("ERROR: ArrayQueue is full.");
+
     }
 
     public double dequeue()
     {
-        increment(front);
-        return arr[front-1];
+        if(size > 0)
+        {
+            double output = arr[front];
+
+            if(size == 1)
+            {
+                front = -1;
+                back = -1;
+            }
+            else
+            {
+                increment(front);
+            }
+
+            size--;
+            return output;
+        }
+        else
+        {
+            System.out.println("ERROR: ArrayQueue is empty.");
+            return 666;
+        }
     }
 
     public boolean isEmpty()
     {
-        return front == back;
+        return size == 0;
     }
 
     public boolean isFull()
     {
-        return (front == 0 && back == arr.length-1) || (back == front - 1);
+        return size == arr.length;
+        //return (front == 0 && back == arr.length-1) || (back == front - 1);
     }
 
     public double peek()
     {
-        return arr[front];
+        if(size > 0)
+            return arr[front];
+        else
+        {
+            System.out.println("ERROR: ArrayQueue is empty.");
+            return 666;
+        }
     }
 
     public int size()
     {
-
+        return size;
     }
 
     public String toString()
     {
+        String output = "[";
+        int index = front;
+        for(int count = 0; count < size-1; count++)
+        {
+            output += arr[index] + ", ";
+            increment(index);
+        }
+        output += arr[index] + "]";
 
+        return output;
     }
 
     private void increment(int index)
     {
         if(index < arr.length-1)
-        {
             index++;
-        }
         else
-        {
             index = 0;
-        }
     }
 }
